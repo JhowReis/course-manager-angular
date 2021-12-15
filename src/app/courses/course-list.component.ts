f84c01d8b705fb2b223d1f098e7f3fc0058da27f
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-12-15 17:33:06 -0300

    import bootstrap

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 
  @Component({
   selector: 'app-course-list',
-  templateUrl: './course-list.component.html'
+  templateUrl: 'course-list.component.html'
  })
 
  export class CourseListComponent {
