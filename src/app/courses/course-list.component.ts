8da7907df5a797893e2d1039ceff35752d9f95e0
Jonathan Reis <jonathan.linkedin2019@gmail.com>
2021-12-15 18:20:41 -0300

    class Course created and table in list.component.html

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,11 +1,42 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Course } from './course'
 
 
- @Component({
-  selector: 'app-course-list',
-  templateUrl: 'course-list.component.html'
- })
+@Component({
+    selector: 'app-course-list',
+    templateUrl: 'course-list.component.html'
+})
 
- export class CourseListComponent {
+export class CourseListComponent implements OnInit {
+    courses: Course[] = [];
 
- }
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular: Forms',
+                imageUrl: '',
+                price: 99.99,
+                code: 'XPS-8769',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'December, 15, 2021'
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 45.99,
+                code: 'LKL-4523',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'December, 15, 2021'
+            }
+        ]
+
+
+    }
+
+
+
+}
